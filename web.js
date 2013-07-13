4f58ea5acb15d3c770ca813f93d28073b7399dc5
Thiago von Sydow <thiagovs05@gmail.com>
2013-07-13 01:16:45 +0000

    added index.html and modified web.js

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var hello = fs.readFileSync("index.html");
+  var buffer = new Buffer(hello, "utf-8");
+  response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
